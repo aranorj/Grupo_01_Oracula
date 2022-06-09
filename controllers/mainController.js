9a221723610dd4d1bdf7ea33cf177c6c8853b606
Your Name <you@example.com>
2022-06-09 13:30:15 -0300

    prueba de partials en clase

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,10 +1,11 @@
 const path = require('path');
+const db = require('../src/model/productos.json')
 const homePath = path.join(__dirname, '../src/views/home.html');
 
 const mainController = {
     //home.html
     index: (req, res) => {
-        res.render('home');
+        res.render('home', {db});
     }
 };
 module.exports = mainController;
